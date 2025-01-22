403b85f1bf8e4dc43a4fcdde74d4b06670392c97
jiuyi.dream <jiuyi.dream@gmail.com>
2025-01-23 01:21:56 +0800

    net tests

---
+++ src/test/golang/channel_test.go
@@ -0,0 +1,42 @@
+package golang
+
+import (
+	"fmt"
+	"testing"
+	time2 "time"
+)
+
+func TestChannel0(t *testing.T) {
+	c := make(chan string)
+	sendChan(c, time2.Now())
+	receiveChan(c)
+
+	select {}
+}
+
+func sendChan(c chan string, start time2.Time) {
+	go func() {
+		for {
+			c <- "a"
+			if t := time2.Now().UnixNano() - start.UnixNano(); t > 100_000 {
+				fmt.Println("will close the chan")
+				close(c)
+				fmt.Println("closed the chan")
+				break
+			}
+			time2.Sleep(1 * time2.Second)
+		}
+	}()
+}
+
+func receiveChan(c chan string) {
+	go func() {
+		for {
+			v, ok := <-c
+			if !ok {
+				panic("chan closed")
+			}
+			println("v is %v", v)
+		}
+	}()
+}
--- src/test/golang/dial_test.go
+++ src/test/golang/dial_test.go
@@ -1,4 +1,4 @@
-package test
+package golang
 
 import (
 	"fmt"
