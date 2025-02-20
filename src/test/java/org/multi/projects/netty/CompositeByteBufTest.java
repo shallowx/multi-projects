b9131518da7a99c3875fa87d71393ee712c7e91a
jiuyi.dream <jiuyi.dream@gmail.com>
2025-02-20 21:02:12 +0800

    Netty ByteBuf tests

--- src/test/java/org/multi/projects/netty/CompositeByteBufTest.java
+++ src/test/java/org/multi/projects/netty/CompositeByteBufTest.java
@@ -1,9 +1,6 @@
 package org.multi.projects.netty;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
-import io.netty.buffer.CompositeByteBuf;
-import io.netty.buffer.Unpooled;
+import io.netty.buffer.*;
 import org.junit.jupiter.api.Test;
 
 public class CompositeByteBufTest {
@@ -23,6 +20,37 @@ public class CompositeByteBufTest {
         byteBuf1.writeByte(33);
         byteBuf1.writeByte(44);
 
+        compositeByteBuf.addFlattenedComponents(true, byteBuf);
+        compositeByteBuf.addFlattenedComponents(true, byteBuf1);
+
+        System.out.println(compositeByteBuf.toString());
+        System.out.println(compositeByteBuf.readerIndex());
+        System.out.println(compositeByteBuf.writerIndex());
+        System.out.println(compositeByteBuf.capacity());
+        System.out.println(compositeByteBuf.maxCapacity());
+
+        System.out.println(compositeByteBuf.readByte());
+        compositeByteBuf.skipBytes(3);
+        System.out.println(compositeByteBuf.readByte());
+
+        compositeByteBuf.release();
+    }
+
+    @Test
+    public void testtestCompositeByteBuf0() {
+        CompositeByteBuf compositeByteBuf = ByteBufAllocator.DEFAULT.compositeDirectBuffer(Integer.MAX_VALUE);
+        PooledByteBufAllocator pooledByteBufAllocator = PooledByteBufAllocator.DEFAULT;
+        ByteBuf byteBuf = pooledByteBufAllocator.directBuffer();
+        byteBuf.writeByte(1);
+        byteBuf.writeByte(2);
+        byteBuf.writeByte(3);
+        byteBuf.writeByte(4);
+
+        ByteBuf byteBuf1 = pooledByteBufAllocator.directBuffer();
+        byteBuf1.writeByte(11);
+        byteBuf1.writeByte(22);
+        byteBuf1.writeByte(33);
+        byteBuf1.writeByte(44);
         compositeByteBuf.addFlattenedComponents(true, byteBuf);
         compositeByteBuf.addFlattenedComponents(true, byteBuf1);
 
