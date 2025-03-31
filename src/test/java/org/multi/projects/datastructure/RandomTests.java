5c3a68e6b7bc6eba43463e2d74feced7662de925
jiuyi.dream <jiuyi.dream@gmail.com>
2025-03-31 22:08:13 +0800

    Netty ByteBuf tests

--- src/test/java/org/multi/projects/datastructure/RandomTests.java
+++ src/test/java/org/multi/projects/datastructure/RandomTests.java
@@ -2,6 +2,9 @@ package org.multi.projects.datastructure;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -10,15 +13,19 @@ public class RandomTests {
     @Test
     public void testRandom() {
         Random r = new Random();
+        List<Integer> lists = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            System.out.println(r.nextInt(11));
+            lists.add(r.nextInt(11));
         }
+        System.out.println(lists);
 
         System.out.println("-----------------");
 
+        lists.clear();
         ThreadLocalRandom random = ThreadLocalRandom.current();
         for (int i = 0; i < 100; i++) {
-            System.out.println(random.nextInt(3,11));
+            lists.add(random.nextInt(3,11));
         }
+        System.out.println(lists);
     }
 }
